1f6ecf79acc52ab9a4dadc8854db54ac6f04f45c
Akshat Gupta <akshat2032003@gmail.com>
2026-04-12 14:38:19 +0530

    Added itemRoute

--- src/main/java/com/lostfound/lostFound/service/impl/ItemServiceImpl.java
+++ src/main/java/com/lostfound/lostFound/service/impl/ItemServiceImpl.java
@@ -2,35 +2,45 @@ package com.lostfound.lostFound.service.impl;
 
 import com.lostfound.lostFound.model.Item;
 import com.lostfound.lostFound.repository.ItemRepo;
+import com.lostfound.lostFound.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public class ItemServiceImpl {
+@Service
+public class ItemServiceImpl implements ItemService {
+
     @Autowired
     private ItemRepo itemRepository;
 
+    @Override
     public List<Item> getAllItems() {
         return itemRepository.findAll();
     }
 
+    @Override
     public Item getItemById(Long id) {
         return itemRepository.findById(id).orElseThrow();
     }
 
+    @Override
     public Item createItem(Item item) {
         return itemRepository.save(item);
     }
 
+    @Override
     public Item updateItem(Long id, Item updatedItem) {
         Item item = getItemById(id);
-        item.setItemName(updatedItem.getItemName());
 
+        item.setItemName(updatedItem.getItemName());
         item.setItemLocation(updatedItem.getItemLocation());
         item.setStatus(updatedItem.getStatus());
+
         return itemRepository.save(item);
     }
 
+    @Override
     public void deleteItem(Long id) {
         itemRepository.deleteById(id);
     }
